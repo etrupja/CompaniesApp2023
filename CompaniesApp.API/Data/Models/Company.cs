e91caf200a2c9b7f5eb2262eae0815bb92d9ed2b
“Ervis <ervistrupja@outlook.com>
2023-11-06 09:04:46 +0100

    Add project files.

---
+++ CompaniesApp.API/Data/Models/Company.cs
@@ -0,0 +1,10 @@
+﻿using CompaniesApp.API.Data.Base;
+
+namespace CompaniesApp.API.Data.Models
+{
+    public class Company : EntityBase
+    {
+        public string Name { get; set; }
+        public int EstablishedYear { get; set; }
+    }
+}
